b4f1a2cdeae34848b631a2bd39f848b2878d22af
nandhagopalakrishna <nandhagopalakrishnan3@gmail.com>
2025-02-06 16:39:44 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Import your actual component - adjust the import path as needed
+import SignIn from './components/SignIn';  // or whatever your component is named
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<YourMainComponent />} />
-        <Route path="/signin" element={<YourMainComponent />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
